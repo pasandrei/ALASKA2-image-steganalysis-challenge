cfc5cda6ba435bf9cb0f06ee7f2e937d6f2df638
Andrei-Sorin Popovici <pas.andrei@yahoo.com>
2020-07-17 15:21:42 +0300

    Disabled weight decay for batch norm and biases

--- main.py
+++ main.py
@@ -97,7 +97,16 @@ def train(train_loop_func, args, logger):
         torch.cuda.set_device(args.local_rank)
     model = model.cuda()
 
-    optimizer = torch.optim.AdamW(model.parameters(), lr=args.learning_rate)
+    param_optimizer = list(model.named_parameters())
+    no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+    optimizer_grouped_parameters = [
+        {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+         'weight_decay': 0.001},
+        {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+         'weight_decay': 0.0}
+    ]
+
+    optimizer = torch.optim.AdamW(optimizer_grouped_parameters, lr=args.learning_rate)
 
     if args.amp:
         model, optimizer = amp.initialize(model, optimizer, opt_level='O2')
